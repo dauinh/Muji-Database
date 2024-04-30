3580219080fc44dabf8ab4959f31c7e466e19dcf
utran0612 <utran@conncoll.edu>
2024-04-30 11:07:44 -0400

    initial connection to DB

---
+++ index.py
@@ -0,0 +1,22 @@
+import mysql.connector
+# importing os module for environment variables
+import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv, dotenv_values 
+
+# loading variables from .env file
+load_dotenv() 
+print(os.getenv("USERNAME"), os.getenv("PASSWORD"))
+
+cnx = mysql.connector.connect(user=os.getenv("USERNAME"), password=os.getenv("PASSWORD"),
+                              host='136.244.224.221',
+                              database='com303fplu')
+
+cursor = cnx.cursor()
+
+query = "SELECT * FROM store"
+
+cursor.execute(query)
+
+results = cursor.fetchall()
+print(results)
