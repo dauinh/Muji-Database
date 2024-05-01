bd02c26a65623a59f6b09aef6a7c72d0a9827cc2
utran0612 <utran@conncoll.edu>
2024-05-01 14:39:09 -0400

    build functions in add_inventory and build CLI menu

--- main.py
+++ main.py
@@ -1,65 +1,94 @@
-import queries
+import queries, add_inventory
 
 def main():
     print('Welcome to Muji Database!')
-    print("Let's explore the database! Select one of these questions:")
-    chosen_num = None
-    
-    print("1. What is the current inventory of a specific product at a particular store?")
-    print("2. What are the 20 top-selling products at each store?")
-    print("3. Which store has the highest total sales revenue?")
-    print("4. What are the 5 stores with the most sales so far this year?")
-    print("5. How many customers are currently enrolled in the frequent-shopper program?")
-    print("6. What is the average order value for online orders compared to in-store purchases?")
-    print("7. Which products have the highest profit margin across all stores?")
-    print("8. How does the sales performance of a particular product compare between different store locations?")
-    print("9. Which store locations have the highest percentage of repeat customers?")
-    print("10. What are the most popular product combinations purchased together by customers?")
-    print("Or enter # to quit")
-    chosen_num = input("Enter the number or # to quit: ")
-    while chosen_num != '#':
-        match chosen_num:
-            case '1':
-                product_id = input("Enter the product Id: ")
-                store_id = input("Enter the store Id: ")
-                res = queries.current_inventory_of_product_at_store(product_id, store_id)
-                print("Current inventory of product at store:", res)
-            case '2':
-                store_id = input("Enter the store Id: ")
-                res = queries.top_selling_products_at_store(store_id)
-                print("Top selling products at store:", res)
-            case '3':
-                store_id = input("Enter the store Id: ")
-                res = queries.store_with_highest_total_sales_revenue()
-                print("Store with highest total sales revenue:", res)
-            case '4':
-                res = queries.stores_with_most_sales_this_year()
-                print("Stores with most sales this year:", res)
-            case '5':
-                res = queries.number_of_customers_in_frequent_shopper_program()
-                print("Number of customers in frequent shopper program:", res)
-            case '6':
-                res = queries.average_order_value_comparison()
-                print("Average order value comparison:", res)
-            case '7':
-                res = queries.products_with_highest_profit_margin()
-                print("Products with highest profit margin:", res)
-            case '8':
-                product_id = input("Enter product id: ")
-                res = queries.sales_performance_of_product_across_stores(product_id)
-                print("Sales performance of product across stores:", res)
-            case '9':
-                res = queries.stores_with_highest_percentage_of_repeat_customers()
-                print("Stores with highest percentage of repeat customers:", res)
-            case '10':
-                res = queries.most_popular_product_combinations()
-                print("Most popular product combinations:", res)
+    print("Let's explore the database! What do you wanna do today?")
+    option = '#'
+    while option == '#':
+        print("1. View performance")    
+        print("2. Update/remove records in database")
+        option = input("Enter a number or q to quit: ")
+        while option != '#' and option != 'q':
+            chosen_num = '#'
+            if option == '1':
+                while chosen_num == '#' or chosen_num != 'q':
+                    print("Select on of the questions below:")
+                    print("1. What is the current inventory of a specific product at a particular store?")
+                    print("2. What are the 20 top-selling products at each store?")
+                    print("3. Which store has the highest total sales revenue?")
+                    print("4. What are the 5 stores with the most sales so far this year?")
+                    print("5. How many customers are currently enrolled in the frequent-shopper program?")
+                    print("6. What is the average order value for online orders compared to in-store purchases?")
+                    print("7. Which products have the highest profit margin across all stores?")
+                    print("8. How does the sales performance of a particular product compare between different store locations?")
+                    print("9. Which store locations have the highest percentage of repeat customers?")
+                    print("10. What are the most popular product combinations purchased together by customers?")
+                    chosen_num = input("Enter the number or q to go back to main menu: ")
+                    if chosen_num == 'q':
+                        option = '#'
+                        break
+                    while chosen_num != '#':
+                        match chosen_num:
+                            case '1':
+                                product_id = input("Enter the product Id: ")
+                                store_id = input("Enter the store Id: ")
+                                res = queries.current_inventory_of_product_at_store(product_id, store_id)
+                                print("Current inventory of product at store:", res)
+                            case '2':
+                                store_id = input("Enter the store Id: ")
+                                res = queries.top_selling_products_at_store(store_id)
+                                print("Top selling products at store:", res)
+                            case '3':
+                                res = queries.store_with_highest_total_sales_revenue()
+                                print("Store with highest total sales revenue:", res)
+                            case '4':
+                                res = queries.stores_with_most_sales_this_year()
+                                print("Stores with most sales this year:", res)
+                            case '5':
+                                res = queries.number_of_customers_in_frequent_shopper_program()
+                                print("Number of customers in frequent shopper program:", res)
+                            case '6':
+                                res = queries.average_order_value_comparison()
+                                print("Average order value comparison:", res)
+                            case '7':
+                                res = queries.products_with_highest_profit_margin()
+                                print("Products with highest profit margin:", res)
+                            case '8':
+                                product_id = input("Enter product id: ")
+                                res = queries.sales_performance_of_product_across_stores(product_id)
+                                print("Sales performance of product across stores:", res)
+                            case '9':
+                                res = queries.stores_with_highest_percentage_of_repeat_customers()
+                                print("Stores with highest percentage of repeat customers:", res)
+                            case '10':
+                                res = queries.most_popular_product_combinations()
+                                print("Most popular product combinations:", res)
+                        chosen_num = input("Enter a number or # to view the menu: ")
+                        
+            elif option == '2':
+                while chosen_num == '#' or chosen_num != 'q':
+                    print("Select on of the options below:")
+                    print("1. Add inventory to the warehouse")
+                    print("2. Add inventory to a store")
+                    print("3. Remove inventory")
+                    print("4. Shift inventory")
+                    chosen_num = input("Enter a number or q to go back to main menu: ")
 
+                    if chosen_num == 'q':
+                        option = '#'
+                        break
+
+                    while chosen_num != "#":
+                        match chosen_num:
+                            case '1':
+                                add_inventory.add_inventory_to_product()
+                            case '2':
+                                add_inventory.add_inventory_to_store()
+                        chosen_num = input("Enter a number or # to view the menu: ")
+            # option = input("Enter a number or # to quit - line 80:")
         
-        chosen_num = input("Enter the number or # to quit: ")
-            
-
-
+        if option == 'q':
+            break
 
 
 if __name__ == "__main__":
